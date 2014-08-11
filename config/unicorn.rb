e7a132dd3b46a65819aaa90c3e169640374f64fc
lobotomy.me <root@lobotomy.me>
2014-08-11 23:03:41 +0400

    added unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+working_directory "/var/www/otprec"
+pid "/var/www/otprec/pids/unicorn.pid"
+stderr_path "/var/www/otprec/log/unicorn.log"
+stdout_path "/var/www/otprec/log/unicorn.log"
+listen "/tmp/unicorn.otprec.sock"
+worker_processes 2
+timeout 30
