27991485435510dcc16438dbeab9b4fb2c8ce90b
lobotomy.me <root@lobotomy.me>
2014-09-09 00:44:41 +0400

    small fix

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,4 @@
 class Record < ActiveRecord::Base
   validates  :text, :url, :end_date, presence: true
-  validates  :url, format: { with: /[a-z0-9]{40}/, message: "Wrong format of short url !!" }
+  validates  :url, format: { with: /[a-z0-9]{40}/, message: "Wrong format of url !!" }
 end
