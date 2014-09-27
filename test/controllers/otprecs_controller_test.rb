1ff32ddf117f88b75d7cc62b23e2281c1e621ac5
lobotomy.me <root@lobotomy.me>
2014-09-28 01:32:23 +0400

    fixed default tests

--- test/controllers/otprecs_controller_test.rb
+++ test/controllers/otprecs_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OtprecControllerTest < ActionController::TestCase
+class OtprecsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
